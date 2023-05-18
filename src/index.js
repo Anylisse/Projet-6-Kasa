6c6083ba568c43f0ad64dfdafb921abc5d40f785
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-05-18 15:42:35 +0200

    Création des routes et suppression fichiers inutilisés

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
+import Home from "./pages/Home";
+import Housing from "./pages/Housing";
+
+//Routes pour les différentes pages//
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="about" element={<About />}>
+          <Route path="home" element={<Home />} />
+          <Route path ="housing" element= {<Housing />}/>
+          <Route path="*" element={<Error404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+  
