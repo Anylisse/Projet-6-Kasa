67ede997081a3c4ba92312ad9dbb1b3e9ad3f198
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-05-19 14:17:04 +0200

    Modifications des routes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Error404 from "./pages/Error404";
-import Home from "./pages/Home";
-import Housing from "./pages/Housing";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-//Routes pour les différentes pages//
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="about" element={<About />}>
-          <Route path="home" element={<Home />} />
-          <Route path ="housing" element= {<Housing />}/>
-          <Route path="*" element={<Error404 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-  
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
