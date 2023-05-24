2a42dd51665023a0d6087b966589303ea043b3bd
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-05-24 16:22:22 +0200

    Création Collapse et mise en forme CSS

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import arrowUp from "../assets/upArrow.png";
+import arrowDown from "../assets/downArrow.png";
+import "../styles/Collapse.css";
+
+function Collapse(props) {
+    const [open, setOpen] = useState(false);
+
+    const toggleCollapse = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div className="collapse">
+            <div className={props.classTitle} onClick={toggleCollapse}>
+                <h3 className="collapse-title">{props.title}</h3>
+                {open ? (
+                    <img src={arrowUp} alt="Flèche vers le haut"/>
+                ) : (
+                    <img src={arrowDown} alt="Flèche vers le bas"/>
+                )}
+            </div>
+            {open && (
+                    <p className={props.classText}>{props.text}</p>
+            )}
+        </div>
+    );
+}
+
+export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,45 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import banner from "../assets/aboutImg.webp";
+import Collapse from "../components/Collapse";
 import Footer from "../components/Footer";
+import "../styles/About.css";
 
 function About() {
     return (
-        <>
+        <div>
             <Header />
-            <Banner />
+
+            <Banner src={banner} alt="Bannière à propos"/>
+
+            <Collapse title="Fiabilité"
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale. 
+            Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+            classTitle="title-collapse"
+            classText="text-collapse"/>
+            
+            <Collapse title="Respect"
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+            Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            classTitle="title-collapse"
+            classText="text-collapse"/>
+
+            <Collapse title="Service"
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
+            N'hésitez pas à nous contacter si vous avez la moindre question."
+            classTitle="title-collapse"
+            classText="text-collapse"/>
+
+            <Collapse title="Sécurité"
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond
+            au critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes
+            de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+            classTitle="title-collapse"
+            classText="text-collapse"/>
+
             <Footer />
-        </>
+        </div>
     );
 }
 
